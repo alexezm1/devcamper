2f9d8817a96cd3345b2813cc4477f7910ffce710
Alejandro Zamora <alex_speed_96@hotmail.com>
2021-03-12 00:14:26 -0700

    POST request Bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -12,7 +12,7 @@ const BootcampSchema = new mongoose.Schema({
   description: {
     type: String,
     required: [true, "Please add a description"],
-    maxlength: [50, "Description can not be more than 500 characters"],
+    maxlength: [500, "Description can not be more than 500 characters"],
   },
   webiste: {
     type: String,
@@ -41,11 +41,9 @@ const BootcampSchema = new mongoose.Schema({
     type: {
       type: String,
       enum: ["Point"],
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: "2dsphere",
     },
     formattedAddress: String,
