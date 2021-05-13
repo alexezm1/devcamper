618701718907b7c07438b5e741d7bcb260c702af
Alejandro Zamora <alex_speed_96@hotmail.com>
2021-05-12 21:32:34 -0700

    bootcamp filter, select, sorting and pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,10 +7,74 @@ const geocoder = require("../utils/geocoder");
 //@route        GET /api/v1/bootcamps
 //@access       Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  //Copy req.query
+  const reqQuery = { ...req.query };
+
+  //Array of fields to exclude
+  const removeFields = ["select", "sort", "limit", "page"];
+
+  //Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  //Create query String
+  let queryString = JSON.stringify(reqQuery);
+
+  //Create operators like {$gt, $gte. etc}
+  queryString = queryString.replace(
+    /\b{gt|gte|lt|lte|in}\b/g,
+    (match) => `$${match}`
+  );
+
+  //Finding resouorce
+  query = Bootcamp.find(JSON.parse(queryString));
+
+  //Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  //Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  //Executing querys
+  const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
